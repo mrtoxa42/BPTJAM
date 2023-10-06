e85ca5dd548b2d809cc40d7ffbaac43e50a2ac35
Emre <emreee.dindar@gmail.com>
2023-10-07 02:26:04 +0300

    Boss
    Boss1 Animasyonları tamamlandı

--- Scripts/Boss/Boss1.gd
+++ Scripts/Boss/Boss1.gd
@@ -1,16 +1,45 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var hp = 300
+var CurrentAni = "Down"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+
+func _process(delta):
+	$HealthBar.value = hp
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if  anim_name == "BossMoveDown":
+		$AnimationPlayer.play("Attack")
+		CurrentAni = "Down"
+	if anim_name == "BossMoveRight" :
+		$AnimationPlayer.play("Attack")
+		CurrentAni = "Right"
+	if anim_name == "BossMoveUp" :
+		$AnimationPlayer.play("Attack")
+		CurrentAni = "Up"
+	if anim_name == "BossMoveLeft" :
+		$AnimationPlayer.play("Attack")
+		CurrentAni = "Left"
+		
+		
+	if anim_name == "GirisAni":
+		$AnimationPlayer.play("BossMoveDown")
+		
+
+	if anim_name == "Attack":
+		if CurrentAni == "Down":
+			$AnimationPlayer.play("BossMoveRight")
+		if CurrentAni == "Right":
+			$AnimationPlayer.play("BossMoveUp")
+		if CurrentAni == "Up":
+			$AnimationPlayer.play("BossMoveLeft")
+		if CurrentAni == "Left" :
+			$AnimationPlayer.play("BossMoveDown")
+		
