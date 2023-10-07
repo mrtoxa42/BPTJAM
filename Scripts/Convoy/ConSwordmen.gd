1be9be6a3fbde08925c2c13522eb5e1157fe654f
mrtoxa42 <betebet16@hotmail.com>
2023-10-08 00:23:01 +0300

    a
    a

---
+++ Scripts/Convoy/ConSwordmen.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+
+
+	
+func _process(delta):
+	if GameManager.selffarmer == null:
+		GameManager.selffarmer = self
+	if GameManager.convoy.village == false:
+		$AnimationPlayer.play("Walk")
+	else:
+		$AnimationPlayer.play("Idle")
+func destroyed():
+	if GameManager.selffarmer == self:
+		GameManager.selffarmer = null
+		queue_free()
