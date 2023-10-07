3193fd812db2fbd4274b3b7dc8718828a339b6bb
mrtoxa42 <betebet16@hotmail.com>
2023-10-07 17:48:02 +0300

    u
    u

--- Scripts/PSoliders/Farmer.gd
+++ Scripts/PSoliders/Farmer.gd
@@ -9,17 +9,25 @@ var currentenemy
 
 func _process(delta):
 	if currentenemy !=null:
-		velocity = Vector2(currentenemy.global_position - global_position)
+		velocity = Vector2(currentenemy.global_position - global_position).normalized()
 	move_and_slide(velocity * speed)
 
 
 
 func _on_FarmerArea_area_entered(area):
-	if area.is_in_group("Enemy"):
-		currentenemy = area
+	pass
 		
 
 func _on_FarmerArea_area_exited(area):
+	pass
+
+
+func _on_DetectedArea_area_entered(area):
+	if area.is_in_group("Enemy"):
+		currentenemy = area
+
+
+func _on_DetectedArea_area_exited(area):
 	if area.is_in_group("Enemy"):
 		if area == currentenemy:
 			currentenemy = null
