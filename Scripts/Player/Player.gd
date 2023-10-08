3ce18ed1d994946847d45dc2a47cf1a5f0289380
mrtoxa42 <betebet16@hotmail.com>
2023-10-08 04:04:38 +0300

    a
    a

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -8,16 +8,25 @@ func _input(event):
 	velocity = Vector2.ZERO
 	if attack == false:
 		if Input.is_action_pressed("A"):
+			$Sprite2.offset.x = -61
+			$Sprite3.offset.x = -61
+			$Sprite2.flip_h = true
+			$Sprite3.flip_h = true
 			velocity.x = -1
 			$AnimationPlayer.play("RunL")
+			
 		elif Input.is_action_pressed("D"):
+			$Sprite2.offset.x = 0
+			$Sprite2.flip_h = false
+			$Sprite3.offset.x = 0
+			$Sprite3.flip_h = false
 			$AnimationPlayer.play("RunR")
 			velocity.x = 1
 		if Input.is_action_pressed("W"):
-			$AnimationPlayer.play("Run")
+			$AnimationPlayer.play("RunR")
 			velocity.y = -1
 		elif Input.is_action_pressed("S"):
-			$AnimationPlayer.play("Run")
+			$AnimationPlayer.play("RunL")
 			velocity.y = 1
 		if velocity == Vector2.ZERO:
 			$AnimationPlayer.play("Idle")
