d5896c713013e55854276e70ebca46beed246245
Emre <emreee.dindar@gmail.com>
2023-10-08 10:22:50 +0300

    a
    a

--- Scripts/Boss/FireBall.gd
+++ Scripts/Boss/FireBall.gd
@@ -4,12 +4,12 @@ var direction = "Left"
 
 func _process(delta):
 	if direction == "Left":
-		position.x -= 2
+		position.x -= 4
 	if direction == "Up":
-		position.x -= 2 
+		position.x -= 4
 		position.y -= 1
 	if direction == "Down":
-		position.x -= 2
+		position.x -= 4
 		position.y += 1
 
 func _on_Timer_timeout():
